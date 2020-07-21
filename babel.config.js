0c1992b23f12e65898594e022d5bf0368037d668
Benedict Tobias <benedict.tobias@yahoo.com>
2020-07-21 15:17:57 -0400

    add babel to transpile

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+// do not use es6 here.
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+  };
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,18 @@
 import sum from './sum';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('object equality', () =>{
+    const personalInformation = {
+        name: 'Ben'
+    }
+
+    personalInformation['age'] = 99;
+
+    expect(personalInformation).toEqual({
+        name: 'Ben',
+        age: 99
+    });
 });
