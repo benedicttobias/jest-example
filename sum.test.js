b2f93bcb91917f2e993e06cbfa8bf9a585c41773
Benedict Tobias <benedict.tobias@yahoo.com>
2020-07-21 15:20:35 -0400

    add not example

--- sum.test.js
+++ sum.test.js
@@ -4,7 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('object equality', () =>{
+test('matcher: object equality', () =>{
     const personalInformation = {
         name: 'Ben'
     }
@@ -14,5 +14,18 @@ test('object equality', () =>{
     expect(personalInformation).toEqual({
         name: 'Ben',
         age: 99
+    });
+});
+
+test('matcher: \'not\' example', () => {
+    const personalInformation = {
+        name: 'Ben'
+    }
+
+    personalInformation['age'] = 99;
+
+    expect(personalInformation).not.toEqual({
+        name: 'Ben',
+        age: -1
     });
 });
