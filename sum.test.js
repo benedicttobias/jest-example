7bfda6a08f3b54e1a5b396f27c813d66d5b06ca1
Benedict Tobias <benedict.tobias@yahoo.com>
2020-07-21 15:22:02 -0400

    using describe to categorize tests

--- sum.test.js
+++ sum.test.js
@@ -4,28 +4,31 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('matcher: object equality', () =>{
-    const personalInformation = {
-        name: 'Ben'
-    }
-
-    personalInformation['age'] = 99;
-
-    expect(personalInformation).toEqual({
-        name: 'Ben',
-        age: 99
+describe('matcher', () => {
+    it('should check for object equality', () =>{
+        const personalInformation = {
+            name: 'Ben'
+        }
+    
+        personalInformation['age'] = 99;
+    
+        expect(personalInformation).toEqual({
+            name: 'Ben',
+            age: 99
+        });
     });
-});
-
-test('matcher: \'not\' example', () => {
-    const personalInformation = {
-        name: 'Ben'
-    }
-
-    personalInformation['age'] = 99;
-
-    expect(personalInformation).not.toEqual({
-        name: 'Ben',
-        age: -1
+    
+    it('should check for \'not\' example', () => {
+        const personalInformation = {
+            name: 'Ben'
+        }
+    
+        personalInformation['age'] = 99;
+    
+        expect(personalInformation).not.toEqual({
+            name: 'Ben',
+            age: -1
+        });
     });
-});
+})
+
